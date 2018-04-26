491d0405ca17b4f41e152ea9e2994d6ee26508d3
Juan Pablo Rojas <32558383+rojasju2@users.noreply.github.com>
2018-04-25 22:49:48 -0400

    Create Script_Report.R

---
+++ Script_Report.R
@@ -0,0 +1,23 @@
+
+
+#Evaluation of different assembly strategies implementend the PATRIC Bioinformatic resource center 
+#CMSE-491
+#Juan Pablo Rojas
+
+library(vegan)
+library(phyloseq)
+library (MASS)
+library(ggplot2) 
+library(grid)
+library(gridExtra)
+library(readr)
+
+assembly_results <- read_csv("~/Bioinformatics & Computation Biology/Class Project/assembly_results.csv")
+View(assembly_results)
+p<-ggplot(data=assembly_results, aes(x=Strategies, y=N50)) +
+  geom_bar(stat="identity")
+p
+
+p2<-ggplot(data=assembly_results, aes(x=Strategies, y=contigs)) +
+  geom_bar(stat="identity")
+p2
